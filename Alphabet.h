54f4278bfe268eaf272ee68c64d156f9e6e48623
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2006-03-24 17:15:26 +0000

    Adding comments

--- Alphabet.h
+++ Alphabet.h
@@ -27,11 +27,13 @@
 
 
 struct alphabet_ {
-  unichar* t[0x10000]; // t['e']= "E\'E"
+  unichar* t[0x10000]; // t['e']= "E{E +'}" where {E+'} stands for the unicode
+                       // character representing the E with accent
   char t2[0x10000];
-       // t2['E'] & 1 -> 'E' is an uppercase letter
-       // t2['e'] & 2 -> 'e' is a lowercase letter
-       // t2['?'] & 1 & 2 -> '?' is a non variable letter (thai, chinese, ...)
+       // t2['E'] = 1 -> 'E' is an uppercase letter
+       // t2['e'] = 2 -> 'e' is a lowercase letter
+       // t2['?'] = (1 & 2) -> '?' is a non variable letter (thai, chinese, ...)
+       // t2['?'] = 0 -> '?' is not a letter
 };
 
 typedef struct alphabet_ Alphabet;
@@ -39,17 +41,17 @@ typedef struct alphabet_ Alphabet;
 Alphabet* load_alphabet(char*);
 void free_alphabet(Alphabet*);
 int is_upper_of(unichar,unichar,Alphabet*);
-int is_equal_or_case_equal(unichar,unichar,Alphabet*);
+int is_equal_or_uppercase(unichar,unichar,Alphabet*);
 int is_lower(unichar,Alphabet*);
 int is_upper(unichar,Alphabet*);
 int is_letter(unichar,Alphabet*);
-int all_in_lower(unichar*,Alphabet*);
-int all_in_upper(unichar*,Alphabet*);
-int all_are_letters(unichar*,Alphabet*);
-int is_equal_ignore_case(unichar*,unichar*,Alphabet*);
+int is_sequence_of_lowercase_letters(unichar*,Alphabet*);
+int is_sequence_of_uppercase(unichar*,Alphabet*);
+int is_sequence_of_letters(unichar*,Alphabet*);
+int is_equal_or_uppercase(unichar*,unichar*,Alphabet*);
 int is_equal_ignore_case_and_quotes(unichar*,unichar*,Alphabet*);
 void turn_portuguese_sequence_to_lowercase(unichar*);
-void replaceLetterByLetterSet(Alphabet*,unichar*,unichar*);
+void replace_letter_by_letter_set(Alphabet*,unichar*,unichar*);
 
 //---------------------------------------------------------------------------
 #endif
