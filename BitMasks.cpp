8f50ab60d2ce770c027696a78d8df0102e540fc1
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2006-11-16 07:25:45 +0000

    Introducing new library BitMasks

---
+++ BitMasks.cpp
@@ -0,0 +1,47 @@
+ /*
+  * Unitex
+  *
+  * Copyright (C) 2001-2006 Université de Marne-la-Vallée <unitex@univ-mlv.fr>
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  * 
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+  *
+  */
+
+#include "BitMasks.h"
+
+
+/**
+ * Tests if the given bit mask is set on the given byte.
+ */
+int is_bit_mask_set(unsigned char value,unsigned char mask) {
+return value & mask;
+}
+
+
+/**
+ * Set the given mask on the given value.
+ */
+void set_bit_mask(unsigned char *value,unsigned char mask) {
+unset_bit_mask(value,mask);
+(*value)=(*value) | mask;
+}
+
+
+/**
+ * Unset the given mask on the given value.
+ */
+void unset_bit_mask(unsigned char *value,unsigned char mask) {
+(*value)=(*value) & (0xFF-mask);
+}
---
+++ BitMasks.h
@@ -0,0 +1,30 @@
+ /*
+  * Unitex
+  *
+  * Copyright (C) 2001-2006 Université de Marne-la-Vallée <unitex@univ-mlv.fr>
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  * 
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+  *
+  */
+
+#ifndef BitMasksH
+#define BitMasksH
+
+int is_bit_mask_set(unsigned char,unsigned char);
+void set_bit_mask(unsigned char*,unsigned char);
+void unset_bit_mask(unsigned char*,unsigned char);
+
+#endif
+
