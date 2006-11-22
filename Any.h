409315a059dba5c6e8c525ca51bab69d00a61460
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2006-11-22 14:13:05 +0000

    Introducing Any.h that can be used to deal with generic values

---
+++ Any.h
@@ -0,0 +1,41 @@
+ /*
+  * Unitex
+  *
+  * Copyright (C) 2001-2006 Université de Marne-la-Vallée <unitex@univ-mlv.fr>
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  * 
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+  *
+  */
+
+#ifndef AnyH
+#define AnyH
+
+#include "unicode.h"
+
+/**
+ * This structure is used to handle a value of any type. If we want to
+ * use a new type, we just have to add the corresponding line in the union.
+ */
+struct any {
+   union {
+      int _int;
+      char _char;
+      unichar _unichar;
+      void* _ptr;
+   };
+};
+
+#endif
+
