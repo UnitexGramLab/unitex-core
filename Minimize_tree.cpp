faeb9c761bff16d48407be46e0c1d574ee88e72c
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2007-02-21 08:46:49 +0000

    Cleaning the MinimizeTree library

--- Minimize_tree.cpp
+++ Minimize_tree.cpp
@@ -19,230 +19,291 @@
   *
   */
 
-//---------------------------------------------------------------------------
-#include <stdio.h>
-
 #include "Minimize_tree.h"
+#include <stdio.h>
 #include "unicode.h"
+#include "Error.h"
+#include "DictionaryTree.h"
 
-//---------------------------------------------------------------------------
+
+/* The maximum height of the tree corresponds to the length of the
+ * longest word in the dictionary */
+#define MAXIMUM_HEIGHT 10000
+
+/* We define here the maximum number of transitions for a given height.
+ * We use a hack to get bigger size on 64bit-architectures */
+#define MAXIMUM_TRANSITIONS ((sizeof(void*) > 4) ? 134217728 : 4194304 )
 
 
-struct node_list* tab_by_hauteur[HAUTEUR_MAX];
-struct dictionary_node_transition* tab_trans[MAX_TRANS];
+/**
+ * We define here a list of transitions in the dictionary tree.
+ * It's here and not in the .h because we don't want to export this
+ * internal data structure.
+ */
+struct transition_list {
+   struct dictionary_node_transition* transition;
+   struct transition_list* next;
+};
 
-void minimize_tree(struct dictionary_node* racine) {
+int compare_nodes(struct dictionary_node_transition*,struct dictionary_node_transition*);
+void init_minimize_arrays(struct transition_list***,struct dictionary_node_transition***);
+void free_minimize_arrays(struct transition_list**,struct dictionary_node_transition**);
+int sort_by_height(struct dictionary_node*,struct transition_list**);
+struct transition_list* new_transition_list(struct dictionary_node_transition*,struct transition_list*);
+int convert_list_to_array(unsigned int,struct transition_list**,
+                          struct dictionary_node_transition**);
+void quicksort(int,int,struct dictionary_node_transition**);
+void merge(int,struct dictionary_node_transition**);
+
+
+
+
+/**
+ * This function takes a dictionary tree and minimizes it using
+ * Dominique Revuz's algorithm.
+ */
+void minimize_tree(struct dictionary_node* root) {
 printf("Minimizing...                      \n");
-init_tab_by_hauteur();
-unsigned int H;
-H=sort_by_height(racine);
+struct transition_list** transitions_by_height;
+struct dictionary_node_transition** transitions;
+init_minimize_arrays(&transitions_by_height,&transitions);
+unsigned int H=sort_by_height(root,transitions_by_height);
 float z;
 for (unsigned int k=0;k<=H;k++) {
-   int size=convert_list_to_array(k);
-   quicksort(0,size-1);
-   fusionner(size);
+   int size=convert_list_to_array(k,transitions_by_height,transitions);
+   quicksort(0,size-1,transitions);
+   merge(size,transitions);
    z=100.0*(float)(k)/(float)H;
    if (z>100.0) z=100.0;
    printf("%2.0f%% completed...    \r",z);
 }
 printf("Minimization done.                     \n");
-free_tab_by_hauteur();
+free_minimize_arrays(transitions_by_height,transitions);
 }
 
 
 
-//
-// this function compares two tree nodes as follow:
-// 1) by the unichar that lead to them
-// 2) by their hash_number (n° of line in INF file)
-// 3) by the transition that get out of them
-//
+/**
+ * This function compares two tree nodes as follow:
+ * 1) by the unichar that lead to them
+ * 2) by their hash_number (n° of line in INF file)
+ * 3) by the transition that get out of them
+ */
 int compare_nodes(struct dictionary_node_transition* a,struct dictionary_node_transition* b) {
 if (a==NULL || b==NULL || a->node==NULL || b->node==NULL) {
-   fprintf(stderr,"Probleme dans compare_nodes\n");
-   getchar();
-   exit(1);
+   fatal_error("Probleme dans compare_nodes\n");
 }
-// then, the hash numbers
+/* If the nodes have not the same INF codes, they are different */
 if (a->node->single_INF_code_list!=NULL && b->node->single_INF_code_list==NULL) return -1;
 if (a->node->single_INF_code_list==NULL && b->node->single_INF_code_list!=NULL) return 1;
-
 if (a->node->single_INF_code_list!=NULL && b->node->single_INF_code_list!=NULL &&
-/*if (*/a->node->INF_code != b->node->INF_code)
+    a->node->INF_code!=b->node->INF_code)
    return (a->node->INF_code - b->node->INF_code);
-// and finally the transitions
+
+/* Then, we compare all the outgoing transitions, two by two */
 a=a->node->trans;
 b=b->node->trans;
 while(a!=NULL && b!=NULL) {
-   // if the unichars are different then nodes are different
+   /* If the 2 current transitions are not tagged by the same 
+    * character, then the nodes are different */
    if (a->letter != b->letter) return (a->letter - b->letter);
-   // if the unichars are equal and destination nodes are different...
+   /* If the characters are equal and destination nodes are different... */
    if (a->node != b->node) return (a->node - b->node);
    a=a->next;
    b=b->next;
 }
 if (a==NULL && b==NULL) {
-   // if the transition lists are equal
+   /* If the transition lists are equal, the nodes are equivalent */
    return 0;
 }
 if (a==NULL) {
-   // if the first list is shorter than the second
+   /* If the first list is shorter than the second */
    return -1;
 }
-// if the first list is longuer then the second
+/* If the first list is longer then the second */
 return 1;
 }
 
 
-
-void init_tab_by_hauteur() {
+/**
+ * We allocate and initialize 2 arrays used by the minimization.
+ */
+void init_minimize_arrays(struct transition_list** *transitions_by_height,
+                          struct dictionary_node_transition** *transitions) {
+(*transitions_by_height)=(struct transition_list**)malloc(MAXIMUM_HEIGHT*sizeof(struct transition_list*));
+if (*transitions_by_height==NULL) {
+   fatal_error("Not enough memory in init_minimize_arrays\n");
+}
 unsigned int i;
-for (i=0;i<HAUTEUR_MAX;i++) {
-   tab_by_hauteur[i]=NULL;
+for (i=0;i<MAXIMUM_HEIGHT;i++) {
+   (*transitions_by_height)[i]=NULL;
+}
+(*transitions)=(dictionary_node_transition**)malloc(MAXIMUM_TRANSITIONS*sizeof(struct dictionary_node_transition*));
+if (*transitions==NULL) {
+   fatal_error("Not enough memory in init_minimize_arrays\n");
 }
 }
 
 
-
-void free_node_list(struct node_list* l) {
-struct node_list* ptr;
+/**
+ * Frees all the given transition list.
+ */
+void free_transition_list(struct transition_list* l) {
+struct transition_list* ptr;
 while (l!=NULL) {
    ptr=l;
-   l=l->suivant;
+   l=l->next;
    free(ptr);
 }
 }
 
 
-
-void free_tab_by_hauteur() {
-int i;
-for (i=0;i<HAUTEUR_MAX;i++) {
-   free_node_list(tab_by_hauteur[i]);
+/**
+ * Frees the 2 arrays used by the minimization.
+ */
+void free_minimize_arrays(struct transition_list** transitions_by_height,
+                          struct dictionary_node_transition** transitions) {
+for (int i=0;i<MAXIMUM_HEIGHT;i++) {
+   free_transition_list(transitions_by_height[i]);
 }
+free(transitions_by_height);
+/* The 'transitions' array is supposed to be allready cleaned */
+free(transitions);
 }
 
 
-
-int sort_by_height(struct dictionary_node* n) {
+/**
+ * This function explores the dictionary and puts its transitions into the 
+ * 'transitions' array. For a given height, the transitions are not sorted,
+ * they will be later in the 'minimize_tree' function.
+ * The function returns the height of the given node.
+ */
+int sort_by_height(struct dictionary_node* n,struct transition_list** transitions_by_height) {
 if (n==NULL) {
-   fprintf(stderr,"Probleme dans sort_by_height\n");
-   exit(1);
+   fatal_error("NULL error in sort_by_height\n");
 }
 if (n->trans==NULL) {
-   // if the node is a leaf
+   /* If the node is a leaf, we have nothing to do */
    return 0;
 }
 struct dictionary_node_transition* trans=n->trans;
-int res=-1;
+int height=-1;
 int k;
 while (trans!=NULL) {
-   k=sort_by_height(trans->node);
-   if (k>res) res=k;
-   insert_trans_in_tab_by_hauteur(trans,k);
+   /* We process recursively the node pointed out by the current transition */
+   k=sort_by_height(trans->node,transitions_by_height);
+   if (k==MAXIMUM_HEIGHT) {
+      fatal_error("Maximum height reached in sort_by_height\n");
+   }
+   /* We insert the transition in the appropriate list */
+   transitions_by_height[k]=new_transition_list(trans,transitions_by_height[k]);
+   /* The height of the current node is the maximum of his sons' heights... */
+   if (height<k) height=k;
    trans=trans->next;
 }
-return 1+res;
+/* ... +1 because of the current node itself */
+return 1+height;
 }
 
 
-
-void insert_trans_in_tab_by_hauteur(struct dictionary_node_transition* trans,int k) {
-  if (k > HAUTEUR_MAX)
-    {
-      fprintf(stderr, "HAUTEUR_MAX=%u reached: exiting!\n", HAUTEUR_MAX);
-      exit(EXIT_FAILURE);
-    }
-  struct node_list* tmp;
-  tmp=new_node_list();
-  tmp->trans=trans;
-  tmp->suivant=tab_by_hauteur[k];
-  tab_by_hauteur[k]=tmp;
+/**
+ * Allocates, initializes and returns a new transition list element.
+ */
+struct transition_list* new_transition_list(struct dictionary_node_transition* transition,
+                                      struct transition_list* next) {
+struct transition_list* t;
+t=(struct transition_list*)malloc(sizeof(struct transition_list));
+if (t==NULL) {
+   fatal_error("Not enough memory in new_transition_list\n");
+}
+t->transition=transition;
+t->next=next;
+return t;
 }
 
 
-
-struct node_list* new_node_list() {
-struct node_list* n;
-n=(struct node_list*)malloc(sizeof(struct node_list));
-n->trans=NULL;
-n->suivant=NULL;
-return n;
+/**
+ * We convert the list of transitions corresponding the given height
+ * into an array, in order to apply the quicksort.
+ */
+int convert_list_to_array(unsigned int height,struct transition_list** transitions_by_height,
+                          struct dictionary_node_transition** transitions) {
+unsigned int size=0;
+struct transition_list* l=transitions_by_height[height];
+struct transition_list* ptr;
+transitions_by_height[height]=NULL;
+while (l!=NULL) {
+   if (size>MAXIMUM_TRANSITIONS) {
+      fatal_error("MAX_TRANS=%u reached: exiting!\n", MAXIMUM_TRANSITIONS);
+   }
+   transitions[size++]=l->transition;
+   ptr=l;
+   l=l->next;
+   free(ptr);
+}
+return size;
 }
 
 
-
-int convert_list_to_array(unsigned int k) {
-  unsigned int size=0;
-  struct node_list* l=tab_by_hauteur[k];
-  struct node_list* ptr;
-  tab_by_hauteur[k]=NULL;
-  while (l!=NULL) {
-    if ( size > MAX_TRANS )
-    {
-      fprintf(stderr, "MAX_TRANS=%u reached: exiting!\n", MAX_TRANS);
-      exit(EXIT_FAILURE);
-    }
-    tab_trans[size++]=l->trans;
-    ptr=l;
-    l=l->suivant;
-    free(ptr);
-  }
-  return size;
-}
-
-
-
-
-//
-// on partitionne le tableau t
-//
-int partition_pour_quicksort(int m, int n) {
+/**
+ * Builds a quicksort partition of the given array.
+ */
+int partition(int start,int end,struct dictionary_node_transition** transitions) {
 struct dictionary_node_transition* pivot;
 struct dictionary_node_transition* tmp;
-int i = m-1;
-int j = n+1;         // indice final du pivot
-pivot=tab_trans[(m+n)/2];
+int i=start-1;
+/* Final pivot index */
+int j=end+1;
+pivot=transitions[(start+end)/2];
 while (true) {
-  do j--;
-  while ((j>(m-1))&&(compare_nodes(pivot,tab_trans[j]) < 0));
-  do i++;
-  while ((i<n+1)&&(compare_nodes(tab_trans[i],pivot) < 0));
-  if (i<j) {
-    tmp=tab_trans[i];
-    tab_trans[i]=tab_trans[j];
-    tab_trans[j]=tmp;
-  } else return j;
+   do j--;
+   while ((j>(start-1))&&(compare_nodes(pivot,transitions[j]) < 0));
+   do i++;
+   while ((i<end+1)&&(compare_nodes(transitions[i],pivot) < 0));
+   if (i<j) {
+      tmp=transitions[i];
+      transitions[i]=transitions[j];
+      transitions[j]=tmp;
+   } else return j;
 }
 }
 
 
-
-void quicksort(int debut, int fin) {
+/**
+ * Sorts the given transition array, according to Dominique Revuz's criteria.
+ */
+void quicksort(int start,int end,struct dictionary_node_transition** transitions) {
 int p;
-if (debut<fin) {
-  p=partition_pour_quicksort(debut,fin);
-  quicksort(debut,p);
-  quicksort(p+1,fin);
+if (start<end) {
+   p=partition(start,end,transitions);
+   quicksort(start,p,transitions);
+   quicksort(p+1,end,transitions);
 }
 }
 
 
-
-void fusionner(int size) {
+/**
+ * 'transitions' is supposed to be sorted, i.e. equivalent transitions
+ * are contiguous. If two transitions point to equivalent nodes, we
+ * redirect the second transition on the first's node and we free the
+ * node that is not pointed anymore.
+ */
+void merge(int size,struct dictionary_node_transition** transitions) {
 int i=1;
-struct dictionary_node_transition* base=tab_trans[0];
+struct dictionary_node_transition* base=transitions[0];
 while (i<size) {
-   if (compare_nodes(base,tab_trans[i])==0) {
-     // if the base trans is equivalent to the current one
-     // then we must destroy the current one's destination node
-     free_dictionary_node(tab_trans[i]->node);
-     // and modify the current one's destination node
-     tab_trans[i]->node=base->node;
-     (base->node->incoming)++;
+   if (compare_nodes(base,transitions[i])==0) {
+      /* If the base transition is equivalent to the current one
+       * then we must destroy the current one's destination node */
+      free_dictionary_node(transitions[i]->node);
+      /* We modify the current one's destination node */
+      transitions[i]->node=base->node;
+      /* And we increase the number of references of the node, in order
+       * to know later when it could be freed */
+      (base->node->incoming)++;
    }
    else {
-      base=tab_trans[i];
+      base=transitions[i];
    }
    i++;
 }
--- Minimize_tree.h
+++ Minimize_tree.h
@@ -19,37 +19,12 @@
   *
   */
 
-//---------------------------------------------------------------------------
 #ifndef Minimize_treeH
 #define Minimize_treeH
-//---------------------------------------------------------------------------
 
 #include "DictionaryTree.h"
 
 
-#define HAUTEUR_MAX 10000
-
-#define MAX_TRANS ((sizeof(void*) > 4) ? 134217728 : 4194304 )
-/* a hack to get bigger size on 64bit-architectures */
-
-struct node_list {
-   struct dictionary_node_transition* trans;
-   struct node_list* suivant;
-};
-
-extern struct node_list* tab_by_hauteur[];
-extern struct dictionary_node_transition* tab_trans[];
-
-
 void minimize_tree(struct dictionary_node*);
-int compare_nodes(struct dictionary_node_transition*,struct dictionary_node_transition*);
-void init_tab_by_hauteur();
-void free_tab_by_hauteur();
-int sort_by_height(struct dictionary_node*);
-void insert_trans_in_tab_by_hauteur(struct dictionary_node_transition*,int);
-struct node_list* new_node_list();
-int convert_list_to_array(unsigned int);
-void quicksort(int, int);
-void fusionner(int);
 
 #endif
