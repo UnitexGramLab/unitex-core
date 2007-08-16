2bdcf5c3064954d2b28cfa202fb0480a94dada86
Alexander Orlov <alexander.orlov@loxal.net>
2007-08-16 13:18:26 +0000

    proper parsing of sentence automata via judy

--- Collocation.cpp
+++ Collocation.cpp
@@ -20,6 +20,12 @@
   *
   */
 
+/*
+ * Author: Burak Arslan (arslan@univ-mlv.fr, plq@gsulinux.org)
+ *         This File contains the collocation extraction api functions.
+ * 
+ */
+
 
 #include <Judy.h>
 
@@ -57,24 +63,33 @@ judy colloc_candidates( struct snt_files *snt, colloc_opt option ) {
 		return 0;
 	}
 
-	Pvoid_t sentence=NULL;
-	Pvoid_t sentenceI;
-	Word_t  sentenceK;  
+	Pvoid_t  sentence=NULL;
+	Pvoid_t  sentenceI;
+	Word_t   sentenceK;  
+	Word_t   sentenceAL;
+
+	PPvoid_t nodes=NULL;
+	Pvoid_t  nodesI;  // iterator
+	Pvoid_t  nodesK;  // key
+	Pvoid_t  nodesS;  // state
+	Word_t   nodesSL; // state length
+	Word_t   nodesKL; // keylength
+	Word_t   nodesAL; // arraylength
+	
 
 	for (i=1; i<=sfst2->number_of_graphs; i++) { // for every sentence
 		j = sfst2->initial_states[i];
 		state = sfst2->states[j];
 		tran = state->transitions;
 
+		sentenceK=0;
+
 		while (state) { 
 			if (tran) {
 				state=sfst2->states[tran->state_number];
-
-				JLI(sentenceI, sentence, (Word_t)tran->state_number);
-				PPvoid_t nodes=(PPvoid_t)sentenceI;
-				Pvoid_t  nodesI;  // iterator
-				Pvoid_t  nodesK;  // key
-				Word_t   nodesKL; // keylength
+				JLI(sentenceI, sentence, sentenceK );
+				sentenceK++;
+				nodes=(PPvoid_t)sentenceI;
 
 				while (tran) {
 					input=sfst2->tags[tran->tag_number]->input;
@@ -97,7 +112,7 @@ judy colloc_candidates( struct snt_files *snt, colloc_opt option ) {
 						}
 					}
 					else {
-						nodesKL=u_strlen(input)*sizeof(unichar);
+						nodesKL=(u_strlen(input)+1)*sizeof(unichar);
 						nodesK=(Pvoid_t)input;
 					}
 
@@ -118,6 +133,44 @@ judy colloc_candidates( struct snt_files *snt, colloc_opt option ) {
 				state=0;
 			}
 		}
+
+
+		sentenceK=0;
+		JLF(sentenceI, sentence, sentenceK);
+		while (sentenceI) {
+			u_printf("%9lu: ", sentenceK);
+
+			nodes=(PPvoid_t)sentenceI;
+			nodesKL=0;
+			nodesS=NULL;
+
+			JHSIF(nodesI, *nodes, nodesS, nodesK, nodesKL);
+			while (nodesI)	{
+				u_printf("%S ", (unichar *)nodesK );
+
+				JHSIN(nodesI, *nodes, nodesS, nodesK, nodesKL);
+			}
+			u_printf("\n");
+
+			JLN(sentenceI, sentence, sentenceK);
+		}
+
+		// freeing loop
+		sentenceK=0;
+//		u_printf ("freed ");
+		JLF(sentenceI, sentence, sentenceK);
+		while (sentenceI) {
+			nodes=(PPvoid_t)sentenceI;
+			JHSFA( nodesAL, *nodes);
+//			u_printf("%lu ", nodesAL);
+			
+			JLN(sentenceI, sentence, sentenceK);
+		}
+		JLFA( sentenceAL, sentence );
+//		u_printf("%lu bytes.\n", sentenceAL);
+
+		u_printf(" - -- --- ==== ===== ==== --- -- - \n");
+		if (i>3) break;
 	}
 
 //u_printf("%d\n", index); exit(0);
