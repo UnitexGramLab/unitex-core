4125ddb8965e3951b8ae574b85090675aad102e4
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2008-11-26 12:49:50 +0000

    Txt2Fst2 has now getopt style options

--- Txt2Fst2.cpp
+++ Txt2Fst2.cpp
@@ -38,22 +38,7 @@
 #include "Error.h"
 #include "Grf2Fst2_lib.h"
 #include "Matches.h"
-
-
-
-void usage() {
-u_printf("%S",COPYRIGHT);
-u_printf("Usage: Txt2Fst2 <text> <alphabet> [-clean] [norm]\n");
-u_printf("     <text> : the text file\n");
-u_printf("     <alphabet> : the alphabet file for the text language\n");
-u_printf("     [-clean] : cleans each sentence automaton, keeping best paths.\n");
-u_printf("     [norm] : the fst2 grammar used to normalize the text automaton.\n\n");
-u_printf("Constructs the text automaton. If the sentences of the text were delimited\n");
-u_printf("with the special tag {S}, the program produces one automaton per sentence.\n");
-u_printf("If not, the text is turned into %d token long automata. The result file\n",MAX_TOKENS_IN_SENTENCE);
-u_printf("named text.fst2 is stored is the text directory.\n\n");
-u_printf("Note that the program will also take into account the file tags.ind, if any.\n\n");
-}
+#include "getopt.h"
 
 
 
@@ -94,16 +79,77 @@ return 1;
 }
 
 
-int main(int argc, char **argv) {
+
+void usage() {
+u_printf("%S",COPYRIGHT);
+u_printf("Usage: Txt2Fst2 <text> <alphabet> [-clean] [norm]\n");
+u_printf("     <text> : the text file\n");
+u_printf("     <alphabet> : the alphabet file for the text language\n");
+u_printf("     [-clean] : cleans each sentence automaton, keeping best paths.\n");
+u_printf("     [norm] : the fst2 grammar used to normalize the text automaton.\n\n");
+u_printf("Constructs the text automaton. If the sentences of the text were delimited\n");
+u_printf("with the special tag {S}, the program produces one automaton per sentence.\n");
+u_printf("If not, the text is turned into %d token long automata. The result file\n",MAX_TOKENS_IN_SENTENCE);
+u_printf("named text.fst2 is stored is the text directory.\n\n");
+u_printf("Note that the program will also take into account the file tags.ind, if any.\n\n");
+}
+
+
+
+int main(int argc,char* argv[]) {
 /* Every Unitex program must start by this instruction,
  * in order to avoid display problems when called from
  * the graphical interface */
 setBufferMode();
 
-if (argc<3 || argc>5) {
+if (argc==1) {
    usage();
    return 0;
 }
+
+const char* optstring=":a:cn:h";
+const struct option lopts[]={
+   {"alphabet", required_argument, NULL, 'a'},
+   {"clean", no_argument, NULL, 'c'},
+   {"normalization_grammar", required_argument, NULL, 'n'},
+   {"help", no_argument, NULL, 'h'},
+   {NULL, no_argument, NULL, 0}
+};
+char alphabet[FILENAME_MAX]="";
+char norm[FILENAME_MAX]="";
+int CLEAN=0;
+int val,index=-1;
+optind=1;
+while (EOF!=(val=getopt_long(argc,argv,optstring,lopts,&index))) {
+   switch(val) {
+   case 'a': if (optarg[0]=='\0') {
+                fatal_error("You must specify a non empty alphabet file name\n");
+             }
+             strcpy(alphabet,optarg);
+             break;      
+   case 'c': CLEAN=1; break;
+   case 'n': if (optarg[0]=='\0') {
+                fatal_error("You must specify a non empty normalization grammar name\n");
+             }
+             strcpy(norm,optarg);
+             break;      
+   case 'h': usage(); return 0;
+   case ':': if (index==-1) fatal_error("Missing argument for option -%c\n",optopt); 
+             else fatal_error("Missing argument for option --%s\n",lopts[index].name);
+   case '?': if (index==-1) fatal_error("Invalid option -%c\n",optopt); 
+             else fatal_error("Invalid option --%s\n",optarg);
+             break;
+   }
+   index=-1;
+}
+
+if (optind!=argc-1) {
+   fatal_error("Invalid arguments: rerun with --help\n");
+}
+if (alphabet[0]=='\0') {
+   fatal_error("You must specify the alphabet file\n");
+}
+
 struct string_hash* tags=new_string_hash();
 /* We insert in any case the epsilon tag in preview of future operations on
  * the text automaton that could need this special tag */
@@ -117,15 +163,15 @@ char text_cod[FILENAME_MAX];
 char dlf[FILENAME_MAX];
 char dlc[FILENAME_MAX];
 char tags_ind[FILENAME_MAX];
-get_snt_path(argv[1],tokens_txt);
+get_snt_path(argv[optind],tokens_txt);
 strcat(tokens_txt,"tokens.txt");
-get_snt_path(argv[1],text_cod);
+get_snt_path(argv[optind],text_cod);
 strcat(text_cod,"text.cod");
-get_snt_path(argv[1],dlf);
+get_snt_path(argv[optind],dlf);
 strcat(dlf,"dlf");
-get_snt_path(argv[1],dlc);
+get_snt_path(argv[optind],dlc);
 strcat(dlc,"dlc");
-get_snt_path(argv[1],tags_ind);
+get_snt_path(argv[optind],tags_ind);
 strcat(tags_ind,"tags.ind");
 load_DELA(dlf,tree);
 load_DELA(dlc,tree);
@@ -136,9 +182,9 @@ if (tag_file!=NULL) {
    tag_list=load_match_list(tag_file,NULL);
    u_fclose(tag_file);
 }
-Alphabet* alph=load_alphabet(argv[2]);
+Alphabet* alph=load_alphabet(alphabet);
 if (alph==NULL) {
-   fatal_error("Cannot open %s\n",argv[2]);
+   fatal_error("Cannot open %s\n",alphabet);
 }
 struct text_tokens* tokens=load_text_tokens(tokens_txt);
 if (tokens==NULL) {
@@ -149,31 +195,16 @@ if (f==NULL) {
    fatal_error("Cannot open %s\n",text_cod);
 }
 char tmp[FILENAME_MAX];
-get_snt_path((const char*)argv[1],tmp);
+get_snt_path(argv[optind],tmp);
 strcat(tmp,"text.fst2");
 FILE* out=u_fopen(tmp,U_WRITE);
 if (out==NULL) {
    u_fclose(f);
    fatal_error("Cannot create %s\n",tmp);
 }
-int CLEAN=0;
 struct normalization_tree* normalization_tree=NULL;
-if (argc==5) {
-   /* If there are optional parameters */
-   if (!strcmp(argv[3],"-clean")) {
-      CLEAN=1;
-   } else {
-      error("Invalid parameter %s\n",argv[3]);
-   }
-   normalization_tree=load_normalization_fst2(argv[4],alph,tokens);
-}
-else
-if (argc==4) {
-   if (!strcmp(argv[3],"-clean")) {
-      CLEAN=1;
-   } else {
-      normalization_tree=load_normalization_fst2(argv[3],alph,tokens);
-   }
+if (norm[0]!='\0') {
+   normalization_tree=load_normalization_fst2(norm,alph,tokens);
 }
 int sentence_number=1;
 int N=0;
@@ -192,10 +223,10 @@ while (read_sentence(buffer,&N,&total,f,tokens->SENTENCE_MARKER)) {
 u_printf("%d sentence%s read\n",sentence_number-1,(sentence_number-1)>1?"s":"");
 fclose(f);
 u_printf("Saving tags...\n");
+unichar temp[4096];
 for (int i=0;i<tags->size;i++) {
-   unichar tmp[1024];
-   escape(tags->value[i],tmp,P_SLASH);
-   u_fprintf(out,"%%%S\n",tmp);
+   escape(tags->value[i],temp,P_SLASH);
+   u_fprintf(out,"%%%S\n",temp);
 }
 u_fprintf(out,"f\n");
 u_fclose(out);
