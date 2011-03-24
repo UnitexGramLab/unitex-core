2397fce9e4c145683262117b223fe8bb03ee7505
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2011-03-24 15:22:03 +0000

    Adding a test to prevent Fst2Txt from using debug fst2

--- Fst2TxtAsRoutine.cpp
+++ Fst2TxtAsRoutine.cpp
@@ -60,6 +60,13 @@ int main_fst2txt(struct fst2txt_parameters* p) {
 		u_fclose(p->f_output);
 		return 1;
 	}
+	if (p->fst2->debug) {
+		error("Cannot use debug fst2 %s\n", p->fst_file);
+		u_fclose(p->f_input);
+		u_fclose(p->f_output);
+		return 1;
+	}
+
 
 	if (p->alphabet_file != NULL && p->alphabet_file[0] != '\0') {
 		p->alphabet = load_alphabet(p->alphabet_file);
