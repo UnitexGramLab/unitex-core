1a84b4d7bca8d2b565574234b5886931f79edd4f
Adrien Durand <adrien.durand@univ-mlv.fr>
2011-08-30 08:49:52 +0000

    Correction of missing states and links problem, useless messages and comments removed

--- Seq2Grf.cpp
+++ Seq2Grf.cpp
@@ -92,72 +92,6 @@ static void usage() {
 	u_printf(usage_Seq2Grf);
 }
 
-///**
-// * This function tries to read a sentence from the given file.
-// * It stops when it finds the EOF, a "{S}" or when MAX_TOKENS_IN_SENTENCE
-// * tokens have been read. All the tokens that compose the sentence are stored
-// * into 'buffer', except the "{S}", if any. The number of these tokens is stored
-// * in '*N'. '*total' contains the whole number of integers read from the file.
-// *
-// * The function returns 1 if a sentence was read; 0 otherwise.
-// */
-//int read_sentence(int* buffer,int *N,int *total,U_FILE* f,int SENTENCE_MARKER) {
-//	*total=0;
-//	*N=0;
-//	u_printf("read_sentence\n");
-//	if (f==NULL) {
-//		u_printf("f NULL\n");
-//		fatal_error("Cannot open file\n");
-//	}else{
-//		u_printf("f not null\n");
-//	}
-//
-//
-//	if (1!=fread(buffer,sizeof(int),1,f)) {
-//		/* If we are at the end of the file */
-//		u_printf("we are at the end of the file \n");
-//		return 0;
-//	}
-//	if (buffer==NULL){
-//		u_printf("buffer NULL\n");
-//	}
-//	u_printf("sentence read\n");
-//	*total=1;
-//	int length;
-//	if (buffer[0]==SENTENCE_MARKER) {
-//		/* If the text starts by a {S}, we don't want to stop there */
-//		length=0;
-//	} else {
-//		length=1;
-//	}
-//	int control=-1;
-//	while (length<MAX_TOKENS_IN_SENTENCE && 1==(control=(int)fread(buffer+length,sizeof(int),1,f)) && buffer[length]!=SENTENCE_MARKER) {
-//		length++;
-//		(*total)++;
-//	}
-//	if (length<MAX_TOKENS_IN_SENTENCE && control==1 && buffer[length]==SENTENCE_MARKER) {
-//		(*total)++;
-//	}
-//	u_printf("read_sentence : done\n");
-//
-//	if (length==0) return 0;
-//	*N=length;
-//	return 1;
-//}
-///**
-// * This function returns the number of space tokens that are in the
-// * given buffer.
-// */
-//int count_non_space_tokens(const int* buffer,int length,int SPACE) {
-//	int n=0;
-//	for (int i=0;i<length;i++) {
-//		if (buffer[i]!=SPACE) {
-//			n++;
-//		}
-//	}
-//	return n;
-//}
-
 const char* optstring_Seq2Grf = ":a:o:hk:q:";
 const struct option_TS lopts_Seq2Grf[] = { { "alphabet", required_argument_TS,
 		NULL, 'a' }, { "output", required_argument_TS, NULL, 'o' }, {
@@ -283,8 +217,6 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 	strcat(tags_ind, "tags.ind");
 	get_snt_path(argv[vars->optind], text_tind);
 	strcat(text_tind, "text.tind");
-	u_printf("\n\n\ntext_tind : %s\n\n\n", text_tind);
-
 	U_FILE* tind = u_fopen(BINARY, text_tind, U_WRITE);
 	struct match_list* tag_list = NULL;
 	load_DELA(&vec,dlf,tree);
@@ -332,7 +264,6 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 	if (f == NULL) {
 		fatal_error("Cannot open %s\n", text_cod);
 	}
-	u_printf("test\n");
 	char text_grf[FILENAME_MAX];
 	get_snt_path(argv[vars->optind], text_grf);
 	U_FILE* grf = u_fopen(&vec, output, U_WRITE);
@@ -348,14 +279,10 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 		return 1;
 	}
 	U_FILE* tfst = u_fopen(&vec, text_tfst, U_WRITE);
-	u_printf("text_tfst 1 : %s\n", text_tfst);
 	if (tfst == NULL) {
 		u_fclose(f);
 		fatal_error("Cannot create %s\n", text_tfst);
 	}
-	u_printf("text_tfst 2 : %s\n", text_tfst);
-
-	u_printf("text_tfst 3 : %s\n", text_tfst);
 	struct hash_table* form_frequencies = new_hash_table(
 			(HASH_FUNCTION) hash_unichar, (EQUAL_FUNCTION) u_equal,
 			(FREE_FUNCTION) free, NULL, (KEYCOPY_FUNCTION) keycopy);
@@ -366,13 +293,11 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 	/* Finally, we save statistics */
 	char tfst_tags_by_freq[FILENAME_MAX];
 	char tfst_tags_by_alph[FILENAME_MAX];
-	u_printf("test 1 \n");
 	get_snt_path(argv[vars->optind], tfst_tags_by_freq);
 	strcat(tfst_tags_by_freq, "tfst_tags_by_freq.txt");
 	get_snt_path(argv[vars->optind], tfst_tags_by_alph);
 	strcat(tfst_tags_by_alph, "tfst_tags_by_alph.txt");
 	U_FILE* f_tfst_tags_by_freq = u_fopen(&vec, tfst_tags_by_freq, U_WRITE);
-	u_printf("test 2 \n");
 	if (f_tfst_tags_by_freq == NULL) {
 		error("Cannot open %s\n", tfst_tags_by_freq);
 	}
@@ -380,19 +305,15 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 	if (f_tfst_tags_by_alph == NULL) {
 		error("Cannot open %s\n", tfst_tags_by_alph);
 	}
-	u_printf("test 3 \n");
 	sort_and_save_tfst_stats(form_frequencies, f_tfst_tags_by_freq,
 			f_tfst_tags_by_alph);
-	u_printf("test 4 \n");
 	u_fclose(f_tfst_tags_by_freq);
 	u_fclose(f_tfst_tags_by_alph);
-	u_printf("test 5 \n");
 	free_hash_table(form_frequencies);
 	//free_Ustring(text);
 	u_fclose(tfst);
 	u_fclose(tind);
 
-	///##########
 	int is_sequence_automaton = 1;
 	int SENTENCE = 1;
 	int size = 10;
@@ -405,11 +326,9 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 		u_printf("tfstFile->automaton==NULL !!! \n");
 	if (tfstFile == NULL)
 		u_printf("tfst NULL\n");
-	u_printf("test 6 \n");
 	Grf* grfObj = sentence_to_grf(tfstFile, fontname, size,
 			is_sequence_automaton);
 	beautify(grfObj, alph);
-	u_printf("test 7 \n");
 	save_Grf(out, grfObj);
 	free_Grf(grfObj);
 	u_fclose(f);
@@ -430,6 +349,7 @@ int main_Seq2Grf(int argc, char* const argv[]) {
 void build_sequences_automaton(U_FILE* f, const struct text_tokens* tokens,
 		const Alphabet* alph, U_FILE* out_tfst, U_FILE* out_tind,
 		int we_must_clean, struct hash_table* form_frequencies) {
+	u_printf("build_sequences_automaton START\n");
 	// New Automaton
 	Tfst* tfst = new_Tfst(NULL, NULL, 0);
 	tfst->current_sentence = 1;
@@ -440,15 +360,11 @@ void build_sequences_automaton(U_FILE* f, const struct text_tokens* tokens,
 	tfst->token_sizes = new_vector_int(0);
 
 	int current_state = 0;
-	// New Initial State and Final state
+	// New Initial State
 	add_state(tfst->automaton); /* initial state */
 
 	set_initial_state(tfst->automaton->states[current_state]);
-	//tfst->tokens
 	current_state++;
-	//	add_state(tfst->automaton); /* final   state */
-	//	current_state++;
-
 
 	struct string_hash* tags = new_string_hash(132);
 	struct string_hash* tmp_tags = new_string_hash(132);
@@ -456,8 +372,6 @@ void build_sequences_automaton(U_FILE* f, const struct text_tokens* tokens,
 	/* The epsilon tag is always the first one */
 	get_value_index(EPSILON_TAG, tmp_tags);
 	get_value_index(EPSILON_TAG, tags);
-	// Structured data used to give a set of parameters to some functions
-	// like explore_*_tree
 	struct info INFO;
 	INFO.tok = tokens;
 	INFO.alph = alph;
@@ -472,27 +386,16 @@ void build_sequences_automaton(U_FILE* f, const struct text_tokens* tokens,
 	Ustring* states = new_Ustring();
 	Ustring* foo = new_Ustring(1);
 	u_fprintf(out_tfst, "0000000001\n");
-	u_printf("Constructing Sequence automaton...\n");
 	if (f == NULL) {
 		u_printf("f NULL\n");
 		fatal_error("Cannot open file\n");
 	}
-	// N : nb tokens dans 1 sequence
-	// total : nb token (non- espace ?) dans toutes les sequences
-	// n_nodes : nb de noeuds à ajouter pour chaque sequence
-	// ? : nb etats dans l'automate ?
 	int nbsentence = 0;
 	int last_transitions[MAX_FST2_STATES][2];
-	//remplacer read_sentence
-	// .txt .alph
-	// readline, tokenization...
 	int n_added_states = 0;
+	bool linked;
 	while (read_sentence(buffer, &N, &total, f, tokens->SENTENCE_MARKER)) {
-		u_printf("\n\n");
-		u_printf("##################################################\n");
-		u_printf("\t\tNBSENTENCE : %d\n", nbsentence);
-		u_printf("##################################################\n");
-
+		linked = false;
 		INFO.buffer = buffer;
 		if (N > INFO.length_max)
 			INFO.length_max = N;
@@ -500,95 +403,46 @@ void build_sequences_automaton(U_FILE* f, const struct text_tokens* tokens,
 		for (i = 0; i < n_nodes; i++) {
 			add_state(tfst->automaton);
 		}
-		while (buffer[N - 1] == tokens->SPACE)
-			N = N - 1;
-		///*while(tokens->token[buffer[N-1]]==".")*/ N=N-1;
+		while (buffer[N - 1] == tokens->SPACE)	N = N - 1;
 		for (int il = 0; il < N; il++) {
-			//			if (buffer[il]!= tokens->SPACE){
 			vector_int_add(tfst->tokens, buffer[il]);
 			int l = u_strlen(tokens->token[buffer[il]]);
 			vector_int_add(tfst->token_sizes, l);
 			u_strcat(foo, tokens->token[buffer[il]], l);
-			u_printf("tokens->token[buffer[%d]]='%S'\n", il,
-					tokens->token[buffer[il]]);
-			//tfst->automaton->number_of_states++;
-			//			}else{
-			//				int l = u_strlen(tokens->token[buffer[il]]);
-			//				u_strcat(foo,tokens->token[buffer[il]],l);
-			//			}
 		}
 		tfst->text = u_strdup(foo->str);
 		/* Transitions */
 		for (i = 0; i < N; i++) {
 			if (buffer[i] == tokens->SENTENCE_MARKER) {
+				u_printf(">>>#>>>\ti = %d ET buffer[%d] = %s\n",i,i,tokens->SENTENCE_MARKER);
 			} else {
 				if (buffer[i] != tokens->SPACE) {
-					//tfst->token_content[current_state]=tokens->token[buffer[i]];
-					//u_printf("1166 - tokens->token[buffer[%d]]=%S\n",i,tokens->token[buffer[i]]);
-					//tfst->token_content[current_state]=tokens->token[buffer[i]];
-					//							tokens->token[buffer[i]][tfst->token_sizes->tab[i]-1]);
-					//					u_sprintf(states,"@STD\n@%S\n@%d.0.0-%d.%d.%d\n.\n",tokens->token[buffer[i]],i,i,
-					//							tfst->token_sizes->tab[i]-1,
-					//							get_length_in_jamo(tokens->token[buffer[i]][tfst->token_sizes->tab[i]-1],korean)-1);
 					u_sprintf(tmp_states, "@STD\n@%S\n@%d.0.0-%d.%d.%d\n.\n",
 							tokens->token[buffer[i]], 0, //i,
 							0, //i,
 							1, //tfst->token_sizes->tab[i]-1,
 							1 //get_length_in_jamo(tokens->token[buffer[i]][tfst->token_sizes->tab[i]-1],korean)-1
 					);
-					//u_strcat(&(tags[current_state],tmp_states);
 					u_strcat(states, tmp_states);
-					u_printf(">>>>new_state<<<<<<\n");
-					u_printf("@STD\n@%S\n@%d.0.0-%d.%d.%d\n.\n",
-							tokens->token[buffer[i]], i, i, 1, //tfst->token_sizes->tab[i]-1,
-							1 //get_length_in_jamo(tokens->token[buffer[i]][tfst->token_sizes->tab[i]-1],korean)-1
-					);
-					u_printf(">>>>>>>>><<<<<<<<<<\n");
 					int tag_number = get_value_index(tmp_states->str, tmp_tags);
-					//					if (tfst->automaton->number_of_states>(sizeof(tfst->automaton->states))){
-					//						add_state(tfst->automaton);
-					//					}
-					if (i == 0) {
-						Transition
-								* trans =
-										tfst->automaton->states[current_state]->outgoing_transitions;
-						add_outgoing_transition(tfst->automaton->states[0],
-								tag_number, current_state + 1);
-						trans
-								= tfst->automaton->states[current_state]->outgoing_transitions;
-						//					while (trans!=NULL) {
-						//						u_printf(" %d %d -",trans->tag_number,trans->state_number);
-						//						trans=trans->next;
-						//					}
-						//					u_printf("\n");
+					if (linked==false) {
+						Transition * trans = tfst->automaton->states[current_state]->outgoing_transitions;
+						add_outgoing_transition(tfst->automaton->states[0], tag_number, current_state + 1);
+					//	trans	= tfst->automaton->states[current_state]->outgoing_transitions;
+						linked=true;
 					} else if (i == N - 1) {
 						last_transitions[nbsentence][0] = current_state;
 						last_transitions[nbsentence][1] = tag_number;
-						//						add_outgoing_transition(tfst->automaton->states[current_state],	1000+tag_number,1);
-						u_printf("\n");
 					} else {
-						//						add_state()
-						Transition
-								* trans =
-										tfst->automaton->states[current_state]->outgoing_transitions;
-						//						add_outgoing_transition(tfst->automaton->states[current_state],	tag_number,current_state+1);
-						//				add_outgoing_transition(tfst->automaton->states[current_state],	tag_number,current_state+1);
+						Transition * trans = tfst->automaton->states[current_state]->outgoing_transitions;
 						add_outgoing_transition(
 								tfst->automaton->states[current_state],
 								get_value_index(tmp_states->str, tmp_tags),
 								current_state + 1);
-						trans
-								= tfst->automaton->states[current_state]->outgoing_transitions;
+						trans = tfst->automaton->states[current_state]->outgoing_transitions;
 						if (trans == NULL) {
 							u_printf("ERROR 3 : transition not added\n");
 						}
-						u_printf(" >");
-						while (trans != NULL) {
-							u_printf(" %d %d -", trans->tag_number,
-									trans->state_number);
-							trans = trans->next;
-						}
-						u_printf("\n");
 					}
 					current_state++;
 				}
@@ -640,11 +494,6 @@ void build_sequences_automaton(U_FILE* f, const struct text_tokens* tokens,
 	minimize(tfst->automaton, 1);
 	save_current_sentence(tfst, out_tfst, out_tind, tags->value, tags->size,
 			NULL);
-	if (tfst->automaton == NULL) {
-		u_printf("tfstFile->automaton==NULL !!!\n");
-	} else {
-		u_printf("GOOD  : tfstFile->automaton=/=NULL !!!\n");
-	}
 
 	close_text_automaton(tfst);
 	free_Ustring(foo);
