9386233b3fb45bfc34c80cf6ab78c85b5d020d2f
Gilles Vollant <gvollant@wanadoo.fr>
2011-11-27 14:52:11 +0000

    quick fix against reading invalid pointer - previous fix change behavior

--- Seq2Grf.cpp
+++ Seq2Grf.cpp
@@ -450,8 +450,8 @@ void add_path(Tfst * tfst,
 	for (int i = 0; i < N; i++) {
 		if (tokens->SENTENCE_MARKER == -1) {
 			u_printf("Warning : tokens->SENTENCE_MARKER == -1, compare aborted against invalid memory pointer usage!!!\n");
-		} else
-		if (seq[i] == tokens->token[tokens->SENTENCE_MARKER]) {
+		}
+		if ((tokens->SENTENCE_MARKER != -1) && (seq[i] == tokens->token[tokens->SENTENCE_MARKER])) {
 			u_printf(">>>#>>>\ti = %d ET buffer[%d] = %s\n",i,i,tokens->SENTENCE_MARKER);
 		} else {
 			u_printf("teest\n");
