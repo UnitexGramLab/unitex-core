26f25423757ced8e3d1d81aa7f66fd65a4f7d737
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2012-06-12 12:13:13 +0000

    Refactoring

--- MF_LangMorpho.h
+++ MF_LangMorpho.h
@@ -43,61 +43,6 @@ namespace unitex {
  * 
  */
 
-/*
-
-
-//maximum number of inflection categories (number, gender etc.)
-#define MAX_CATS 100
-//maximum number of inflection classes (noun, adj, etc.)
-#define MAX_CLASSES 50
-//maximum length of a class name in a dictionary
-#define MAX_CLASS_NAME 10
-//maximum number of values for an inflection category
-#define MAX_CAT_VALUES 500
-//maximum length of a line in the language morhology file
-#define MAX_LANG_MORPHO_LINE 500
-//maximum length of a class, category or value name
-#define MAX_MORPHO_NAME 20
-
-//Structure for an inflection category (cf Inflection/Morphology file)
-//e.g. for English 
-//          name = "Nb"  (number inflection)
-//          no_values = 2 
-//          values = ["sing","pl"] (singular, plural)
-typedef struct {
-  unichar* name;
-  int no_values;
-  unichar* values[MAX_CAT_VALUES];
-} l_category_T;
-
-//Structure for all inflection categories
-typedef struct {
-  int no_cats;     //number of categories
-  l_category_T cats[MAX_CATS];
-} l_cats_T;
-
-//Structure for an inflection class (cf Inflection/Morphology file)
-//e.g. for Polish 
-//          name = "noun"
-//          no_cats = 3 
-//          cats = ((Nb,0),(Case,0),(Gen,1))  // a Polish noun HAS a gender and INFLECTS IN number and case
-typedef struct {
-  unichar* name;   
-  int no_cats; 
-  struct {
-    l_category_T* cat;
-    int fixed;
-  } cats[MAX_CATS];
-} l_class_T;
-
-//From the viewpoint of morphology a language is a list of classes (noun, verb, etc.)
-//which are lists of categories (number, case, gender, etc.) and their domains.
-typedef struct{
-  int no_classes;      //e.g. 7
-  l_class_T classes[MAX_CLASSES];  //e.g. (noun, verb, etc.)
-} l_classes_T;
-
-*/
 
 struct l_morpho_t
 {
