df36403fdfe8b555d8c40d2ba8f08659988d08ac
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2012-07-03 08:24:21 +0000

    Fixing bug in ConcorDiff

--- Diff.cpp
+++ Diff.cpp
@@ -42,13 +42,13 @@ namespace unitex {
  * the associated concordances 'out1' and 'out2'.
  */
 void create_text_concordances(const VersatileEncodingConfig* vec,const char* in1,const char* in2,const char* out1,const char* out2) {
-pseudo_main_Concord(vec,in1,NULL,0,20,40,NULL,"--diff",NULL,NULL,0,0,0);
+pseudo_main_Concord(vec,in1,NULL,0,20,80,NULL,"--diff",NULL,NULL,0,0,0);
 char f[FILENAME_MAX];
 get_path(in1,f);
 strcat(f,"concord.txt");
 af_remove(out1);
 af_rename(f,out1);
-pseudo_main_Concord(vec,in2,NULL,0,20,40,NULL,"--diff",NULL,NULL,0,0,0);
+pseudo_main_Concord(vec,in2,NULL,0,20,80,NULL,"--diff",NULL,NULL,0,0,0);
 af_remove(out2);
 af_rename(f,out2);
 }
@@ -133,10 +133,61 @@ u_fclose(f1);
 u_fclose(f2);
 u_fclose(output);
 /* We remove the tmp files */
-af_remove(concor1);
-af_remove(concor2);
+//af_remove(concor1);
+//af_remove(concor2);
 return 1;
 }
+
+
+/**
+ * Looks in the given list for a match identical to m. In case
+ * of success, it returns 1 and the list is modified so that
+ * its first element is the identical one. Returns 0 otherwise.
+ *
+ * NOTE: the return value is different from look_for_same_outputs!
+ */
+static int look_for_same_outputs_(struct match_list* m,
+							struct match_list* list) {
+struct match_list* head=list;
+while (list!=NULL && compare_matches(&(m->m),&(list->m))==A_EQUALS_B) {
+	if (!u_strcmp(m->output,list->output)) {
+		/* We have an identical match */
+		unichar* tmp=head->output;
+		head->output=list->output;
+		list->output=tmp;
+		return 1;
+	}
+	list=list->next;
+}
+return 0;
+}
+
+
+/**
+ * If a we have at the same position list1=xA and list2=xB,xA
+ * we want to avoid the following diff analysis:
+ *
+ * 1) xA != xB
+ * 2)    != xA
+ *
+ * Instead, we want to reorder the matches so that we have:
+ *
+ * 1) xA == xA
+ * 2)    != xB
+ *
+ * This functions looks for such reordering. If possible,
+ * the concerned outputs are swapped so that the lists' first
+ * items have identical outputs and the function returns 0.
+ * If no identical outputs are found, it returns 1 and leaves the
+ * lists unmodified.
+ */
+int look_for_same_outputs(struct match_list* list1,
+							struct match_list* list2) {
+if (look_for_same_outputs_(list1,list2)) return 0;
+if (look_for_same_outputs_(list2,list1)) return 0;
+return 1;
+}
+
 
 
 /**
@@ -159,14 +210,14 @@ while (!(list1==NULL && list2==NULL)) {
    if (list1==NULL) {
       /* If the first list is empty, then the current match in the second list
        * must be green */
-      print_diff_matches(output,NULL,f2,GREEN);
+      print_diff_matches(output,NULL,f2,GREEN,NULL,list2->output);
       list2=list2->next;
       continue;
    }
    if (list2==NULL) {
       /* If the second list is empty, then the current match in the first list
        * must be green */
-      print_diff_matches(output,f1,NULL,GREEN);
+      print_diff_matches(output,f1,NULL,GREEN,list1->output,NULL);
       list1=list1->next;
       continue;
    }
@@ -174,14 +225,14 @@ while (!(list1==NULL && list2==NULL)) {
       case A_BEFORE_B: {
          /* list1 has no common part with list2:
           * abcd,efgh */
-         print_diff_matches(output,f1,NULL,GREEN);
+         print_diff_matches(output,f1,NULL,GREEN,list1->output,list2->output);
          list1=list1->next;
          break;
       }
       case A_INCLUDES_B: {
          /* list2 is included in list1:
           * abcdef,cdef */
-         print_diff_matches(output,f1,f2,RED);
+         print_diff_matches(output,f1,f2,RED,list1->output,list2->output);
          list1=list1->next;
          list2=list2->next;
          break;
@@ -192,21 +243,21 @@ while (!(list1==NULL && list2==NULL)) {
           *
           * We consider that they are two distinct lines, and we
           * print the first */
-         print_diff_matches(output,f1,NULL,GREEN);
+         print_diff_matches(output,f1,NULL,GREEN,list1->output,list2->output);
          list1=list1->next;
          break;
       }
       case A_AFTER_B: {
          /* list2 has no common part with list1:
           * abcd,efgh */
-         print_diff_matches(output,NULL,f2,GREEN);
+         print_diff_matches(output,NULL,f2,GREEN,list1->output,list2->output);
          list2=list2->next;
          break;
       }
       case B_INCLUDES_A: {
          /* list1 is included in list2:
           * abcd,abcdef */
-         print_diff_matches(output,f1,f2,RED);
+         print_diff_matches(output,f1,f2,RED,list1->output,list2->output);
          list1=list1->next;
          list2=list2->next;
          break;
@@ -216,7 +267,7 @@ while (!(list1==NULL && list2==NULL)) {
           * abcdef,cdefgh
           * We consider that they are two distinct lines, and we
           * print the first */
-         print_diff_matches(output,NULL,f2,GREEN);
+         print_diff_matches(output,NULL,f2,GREEN,list1->output,list2->output);
          list2=list2->next;         
          break;
       }
@@ -224,8 +275,14 @@ while (!(list1==NULL && list2==NULL)) {
          /* list1 == list2:
           * abcd,abcd */
     	 int different_outputs=u_strcmp(list1->output,list2->output);
+    	 if (different_outputs) {
+    		 /* If there are matches with ambiguous outputs, we may find
+    		  * an exact match pair forward in the lists
+    		  */
+    		 different_outputs=look_for_same_outputs(list1,list2);
+    	 }
     	 if (!diff_only || different_outputs) {
-            print_diff_matches(output,f1,f2,different_outputs?VIOLET:BLUE);
+            print_diff_matches(output,f1,f2,different_outputs?VIOLET:BLUE,list1->output,list2->output);
     	 } else {
     		 /* We have to skip the unused lines */
     		 u_fskip_line(f1);
@@ -243,14 +300,21 @@ while (!(list1==NULL && list2==NULL)) {
 /**
  * This function reads one concordance line from 'f', and splits its
  * components into 'left', 'middle' and 'right'.
+ *
+ * IMPORTANT: in order to fix a bug, we may have to reorder some matches. However, we can't
+ *            easily reorder lines in concordance files. So, we indicate the length of the
+ *            expected match instead of relying on the length of the match that was actually read.
+ *            This is why there is the (++total>=60) hack.
  */
-void read_concordance_line(U_FILE* f,unichar* left,unichar* middle,unichar* right,unichar* indices) {
+void read_concordance_line(U_FILE* f,unichar* left,unichar* middle,unichar* right,unichar* indices,int
+		expected_match_length) {
 int i,c;
 i=0;
 while ((c=u_fgetc(f))!='\t') {
    left[i++]=(unichar)c;
 }
 left[i]='\0';
+int total=i+expected_match_length;
 i=0;
 while ((c=u_fgetc(f))!='\t') {
    middle[i++]=(unichar)c;
@@ -258,6 +322,9 @@ while ((c=u_fgetc(f))!='\t') {
 middle[i]='\0';
 i=0;
 while ((c=u_fgetc(f))!='\t') {
+	if (++total>=60) {
+		c='\0';
+	}
    right[i++]=(unichar)c;
 }
 right[i]='\0';
@@ -287,7 +354,8 @@ for (int i=0;i<n;i++) {
  * This function loads concordance lines from 'f1' and/or 'f2' and prints them to
  * 'output' in the given color.
  */
-void print_diff_matches(U_FILE* output,U_FILE* f1,U_FILE* f2,const char* color) {
+void print_diff_matches(U_FILE* output,U_FILE* f1,U_FILE* f2,const char* color,
+		unichar* match1,unichar* match2) {
 unichar left1[MAX_CONTEXT_IN_UNITS];
 unichar middle1[MAX_CONTEXT_IN_UNITS];
 unichar right1[MAX_CONTEXT_IN_UNITS];
@@ -297,11 +365,13 @@ unichar middle2[MAX_CONTEXT_IN_UNITS];
 unichar right2[MAX_CONTEXT_IN_UNITS];
 unichar indices2[MAX_CONTEXT_IN_UNITS];
 if (f1!=NULL) {
-   read_concordance_line(f1,left1,middle1,right1,indices1);
+   read_concordance_line(f1,left1,middle1,right1,indices1,u_strlen(match1));
 }
 if (f2!=NULL) {
-   read_concordance_line(f2,left2,middle2,right2,indices2);
+   read_concordance_line(f2,left2,middle2,right2,indices2,u_strlen(match2));
 }
+if (match1!=NULL) u_strcpy(middle1,match1);
+if (match2!=NULL) u_strcpy(middle2,match2);
 if (!strcmp(color,RED)) {
 	/* If we have one match included in the another, we want to align
 	 * them. We do that by adjusting their left contexts */
