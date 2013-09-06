63178e8efff8694c5df32c536008b3f16e60f493
Gilles Vollant <gvollant@wanadoo.fr>
2013-09-06 00:35:23 +0000

    comment PersistenceInterface

--- PersistenceInterface.h
+++ PersistenceInterface.h
@@ -40,6 +40,37 @@ void standard_unload_persistence_fst2(const char*filename);
 int standard_load_persistence_alphabet(const char*filename,char* persistent_filename_buffer,size_t buffer_size);
 void standard_unload_persistence_alphabet(const char*filename);
 
+/* developper which uses Unitex library, please uses these function for persistence :
+   persistence_public_load_XXX and persistence_public_unload_XXX */
+
+/* persistence_public_load_XXX : load a resource in persistence space
+   first filename : IN parameter, existent file in filespace (hard disk or virtual file system)
+   persistent_filename_buffer is a char output buffer of size buffer_size, where persistence_public_load_XXX will copy
+   the persisted name of resource (derived from filename but not strictly identical, depending of implementation).
+
+   Please use the name returned in persistent_filename_buffer in Unitex Command (Locate...) and in
+   persistence_public_unload_XXX at dealloc
+
+   return 0 if fail, no zero if success
+
+
+   example:
+
+   const char *automatonOriginalString = "/Users/foo/myres/graphs/test.fst2";
+   
+		char PersistedFileName[0x200] = "";
+		if (persistence_public_load_fst2(automatonOriginalString, PersistedFileName, sizeof(PersistedFileName)-1) == 0) {
+			printf("Error while persisting FST2 %s", automatonOriginalString);
+			return false;
+		}
+
+
+		// Do a lot of work, several locate using PersistedFileName as graph
+
+		// when you application will free all memory
+		persistence_public_unload_fst2(PersistedFileName);
+   */
+
 UNITEX_FUNC int UNITEX_CALL persistence_public_load_dictionary(const char*filename,char* persistent_filename_buffer,size_t buffer_size);
 UNITEX_FUNC void UNITEX_CALL persistence_public_unload_dictionary(const char*filename);
 
