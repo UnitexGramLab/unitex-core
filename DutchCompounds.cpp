0d111b1b2dc0b15e7b63b5bfb00b96195733d544
Gilles Vollant <gvollant@wanadoo.fr>
2014-10-22 21:28:24 +0000

    cosmetic fix

--- DutchCompounds.cpp
+++ DutchCompounds.cpp
@@ -1,23 +1,23 @@
 /*
-* Unitex
-*
-* Copyright (C) 2001-2014 Université Paris-Est Marne-la-Vallée <unitex@univ-mlv.fr>
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-*
-*/
+ * Unitex
+ *
+ * Copyright (C) 2001-2014 Université Paris-Est Marne-la-Vallée <unitex@univ-mlv.fr>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ *
+ */
 
 #include "DutchCompounds.h"
 #include "Error.h"
@@ -550,8 +550,8 @@ namespace unitex {
 		const unichar* output_dela_line, struct word_decomposition_list** L, long* nb_word_decomposition_list_possible,
 		int number_of_components, const struct dutch_infos* infos, Ustring* ustr, int base) {
 
-		#define STANDARD_STRING_BUFFER_SIZE (256/1)
-		#define DEC_STRING_BUFFER_SIZE (1024 / 01)
+		#define STANDARD_STRING_BUFFER_SIZE    (256)
+		#define DEC_STRING_BUFFER_SIZE         (1024)
 
 		/* abort if excess list size */
 		if ((*nb_word_decomposition_list_possible) <= 0)
