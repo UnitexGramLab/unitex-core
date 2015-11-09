a5e9f92a70221f57299be3e3866ee8ec3b1818f3
Cristian Martinez <cristian.martinez@univ-paris-est.fr>
2015-11-09 19:01:42 +0000

    add a header regrouping Unitex release information

---
+++ Version.h
@@ -0,0 +1,71 @@
+/*
+ * Unitex
+ *
+ * Copyright (C) 2001-2015 Université Paris-Est Marne-la-Vallée <unitex@univ-mlv.fr>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ *
+ */
+/**
+ * @file      Version.h
+ * @brief     Defines the current version of the Unitex Core
+ *            
+ * @author    Vinber Process
+ * 
+ * @warning   DO NOT CHANGE THIS FILE DIRECTLY! IT WILL BE OVERWRITTEN.
+ *            BUT INSTEAD USE VERSION.H.IN
+ *
+ * @remark    
+ *          
+ * @date      November 09, 2015
+ * 
+ */
+/* ************************************************************************** */
+#ifndef UNITEX_CORE_VERSION_H                                       // NOLINT
+#define UNITEX_CORE_VERSION_H                                       // NOLINT
+/* ************************************************************************** */
+#define UNITEX_VERSION_NAME                "Unitex"
+#define UNITEX_VERSION_NAMESPACE           "unitex"
+/* ************************************************************************** */
+#define UNITEX_VERSION_AUTHOR              "The Unitex Authors"
+#define UNITEX_VERSION_COMPANY             "Universite Paris-Est Marne-la-Vallee"
+#define UNITEX_VERSION_COPYRIGHT           "Copyright (C) 2001-2015"
+#define UNITEX_VERSION_DESCRIPTION_SHORT   "corpus processing suite"
+#define UNITEX_VERSION_DESCRIPTION         "an open source, cross-platform, multilingual, lexicon- and grammar-based corpus processing suite"
+#define UNITEX_VERSION_LICENSE             "LGPL-2.1"
+/* ************************************************************************** */
+#define UNITEX_VERSION_URL_ISSUES          "http://forum.unitexgramlab.org"
+#define UNITEX_VERSION_URL_REPOSITORY      "https://svnigm.univ-mlv.fr/svn/unitex/Unitex-C++"
+#define UNITEX_VERSION_URL_WEBSITE         "http://unitexgramlab.org"
+/* ************************************************************************** */
+#define UNITEX_VERSION_MAJOR_NUMBER        3
+#define UNITEX_VERSION_MINOR_NUMBER        1
+#define UNITEX_VERSION_REVISION_NUMBER     0
+#define UNITEX_VERSION_SUFFIX              "beta"
+#define UNITEX_VERSION_IS_UNSTABLE         1
+/* ************************************************************************** */
+#define UNITEX_VERSION_BUILD_IS_ANONYMOUS  1
+#define UNITEX_VERSION_BUILD_NUMBER        0
+#define UNITEX_VERSION_BUILD_DAY           "?"
+#define UNITEX_VERSION_BUILD_MONTH         "?"
+#define UNITEX_VERSION_BUILD_TIMESTAMP     "?"
+#define UNITEX_VERSION_BUILD_YEAR          "?"
+#define UNITEX_VERSION_BUILD_COMMIT_BRANCH "?"
+#define UNITEX_VERSION_BUILD_COMMIT_DATE   "?"
+#define UNITEX_VERSION_BUILD_COMMIT_HASH   "?"
+#define UNITEX_VERSION_BUILD_COMMIT_TAG    "?"
+/* ************************************************************************** */
+#endif  // UNITEX_CORE_VERSION_H                                    // NOLINT
+
