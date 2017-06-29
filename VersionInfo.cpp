bcfae253655236d090e8ed21123f774a6b53ada5
vahbuna <silver.estla@gmail.com>
2017-06-29 10:55:29 +0200

    [minor feature] Add display build date time
    New feature added in VersionInfo to show: 1) build date in YYYY-MM-DD
    format and 2) build date time

--- VersionInfo.cpp
+++ VersionInfo.cpp
@@ -55,6 +55,8 @@ const char* usage_VersionInfo =
          "  -n/--newrevision: display only SVN new revision number (if available)\n"
          "  -g/--git: display only git hash (if available)\n"
          "  -b/--build_date: display only build date (if available)\n"
+         "  -B/--build_date_yyyy_mm_dd: display only build date (if available) begin with year\n"
+         "  -t/--build_date_time: display only build date and time (if available)\n"
          "  -p/--platform: platform info\n"
          "  -m/--compiler: compiler used to build unitex info\n"
          "  -j/--json: revision and version on json string\n"
@@ -70,12 +72,14 @@ static void usage() {
   u_printf(usage_VersionInfo);
 }
 
-const char* optstring_VersionInfo=":VhmcRrvpjxsubngo:k:q:";
+const char* optstring_VersionInfo=":VhmcRrvpjxsubBtngo:k:q:";
 const struct option_TS lopts_VersionInfo[]= {
   {"copyright",no_argument_TS,NULL,'c'},
   {"version",no_argument_TS,NULL,'v'},
   {"revision",no_argument_TS,NULL,'r'},
   {"build_date",no_argument_TS,NULL,'b'},
+  {"build_date_yyyy_mm_dd",no_argument_TS,NULL,'B'},
+  {"build_date_time",no_argument_TS,NULL,'t'},
   {"newrevision",no_argument_TS,NULL,'n'},
   {"git",no_argument_TS,NULL,'g'},
   {"platform",no_argument_TS,NULL,'p'},
@@ -181,14 +185,175 @@ static void fill_compiler_info(unichar* s) {
 
 
 #ifdef __DATE__
+// see https://stackoverflow.com/questions/11697820/how-to-use-date-and-time-predefined-macros-in-as-two-integers-then-stri
+// Example of __DATE__ string: "Jul 27 2012"
+//                              01234567890
+#define BUILD_YEAR_CH0 (__DATE__[ 7])
+#define BUILD_YEAR_CH1 (__DATE__[ 8])
+#define BUILD_YEAR_CH2 (__DATE__[ 9])
+#define BUILD_YEAR_CH3 (__DATE__[10])
+
+#define BUILD_MONTH_IS_JAN (__DATE__[0] == 'J' && __DATE__[1] == 'a' && __DATE__[2] == 'n')
+#define BUILD_MONTH_IS_FEB (__DATE__[0] == 'F')
+#define BUILD_MONTH_IS_MAR (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'r')
+#define BUILD_MONTH_IS_APR (__DATE__[0] == 'A' && __DATE__[1] == 'p')
+#define BUILD_MONTH_IS_MAY (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'y')
+#define BUILD_MONTH_IS_JUN (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'n')
+#define BUILD_MONTH_IS_JUL (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'l')
+#define BUILD_MONTH_IS_AUG (__DATE__[0] == 'A' && __DATE__[1] == 'u')
+#define BUILD_MONTH_IS_SEP (__DATE__[0] == 'S')
+#define BUILD_MONTH_IS_OCT (__DATE__[0] == 'O')
+#define BUILD_MONTH_IS_NOV (__DATE__[0] == 'N')
+#define BUILD_MONTH_IS_DEC (__DATE__[0] == 'D')
+
+#define BUILD_MONTH_CH0 \
+    ((BUILD_MONTH_IS_OCT || BUILD_MONTH_IS_NOV || BUILD_MONTH_IS_DEC) ? '1' : '0')
+
+#define BUILD_MONTH_CH1 \
+    ( \
+        (BUILD_MONTH_IS_JAN) ? '1' : \
+        (BUILD_MONTH_IS_FEB) ? '2' : \
+        (BUILD_MONTH_IS_MAR) ? '3' : \
+        (BUILD_MONTH_IS_APR) ? '4' : \
+        (BUILD_MONTH_IS_MAY) ? '5' : \
+        (BUILD_MONTH_IS_JUN) ? '6' : \
+        (BUILD_MONTH_IS_JUL) ? '7' : \
+        (BUILD_MONTH_IS_AUG) ? '8' : \
+        (BUILD_MONTH_IS_SEP) ? '9' : \
+        (BUILD_MONTH_IS_OCT) ? '0' : \
+        (BUILD_MONTH_IS_NOV) ? '1' : \
+        (BUILD_MONTH_IS_DEC) ? '2' : \
+        /* error default */    '?' \
+    )
+
+#define BUILD_DAY_CH0 ((__DATE__[4] >= '0') ? (__DATE__[4]) : '0')
+#define BUILD_DAY_CH1 (__DATE__[ 5])
+
+// Example of __TIME__ string: "21:06:19"
+//                              01234567
+
+#define BUILD_HOUR_CH0 (__TIME__[0])
+#define BUILD_HOUR_CH1 (__TIME__[1])
+
+#define BUILD_MIN_CH0 (__TIME__[3])
+#define BUILD_MIN_CH1 (__TIME__[4])
+
+#define BUILD_SEC_CH0 (__TIME__[6])
+#define BUILD_SEC_CH1 (__TIME__[7])
+
+#define COMPUTE_BUILD_YEAR \
+    ( \
+        (__DATE__[ 7] - '0') * 1000 + \
+        (__DATE__[ 8] - '0') *  100 + \
+        (__DATE__[ 9] - '0') *   10 + \
+        (__DATE__[10] - '0') \
+    )
+
+#define COMPUTE_BUILD_DAY \
+    ( \
+        ((__DATE__[4] >= '0') ? (__DATE__[4] - '0') * 10 : 0) + \
+        (__DATE__[5] - '0') \
+    )
+
+#define BUILD_MONTH_IS_JAN (__DATE__[0] == 'J' && __DATE__[1] == 'a' && __DATE__[2] == 'n')
+#define BUILD_MONTH_IS_FEB (__DATE__[0] == 'F')
+#define BUILD_MONTH_IS_MAR (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'r')
+#define BUILD_MONTH_IS_APR (__DATE__[0] == 'A' && __DATE__[1] == 'p')
+#define BUILD_MONTH_IS_MAY (__DATE__[0] == 'M' && __DATE__[1] == 'a' && __DATE__[2] == 'y')
+#define BUILD_MONTH_IS_JUN (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'n')
+#define BUILD_MONTH_IS_JUL (__DATE__[0] == 'J' && __DATE__[1] == 'u' && __DATE__[2] == 'l')
+#define BUILD_MONTH_IS_AUG (__DATE__[0] == 'A' && __DATE__[1] == 'u')
+#define BUILD_MONTH_IS_SEP (__DATE__[0] == 'S')
+#define BUILD_MONTH_IS_OCT (__DATE__[0] == 'O')
+#define BUILD_MONTH_IS_NOV (__DATE__[0] == 'N')
+#define BUILD_MONTH_IS_DEC (__DATE__[0] == 'D')
+
+#define COMPUTE_BUILD_MONTH \
+    ( \
+        (BUILD_MONTH_IS_JAN) ?  1 : \
+        (BUILD_MONTH_IS_FEB) ?  2 : \
+        (BUILD_MONTH_IS_MAR) ?  3 : \
+        (BUILD_MONTH_IS_APR) ?  4 : \
+        (BUILD_MONTH_IS_MAY) ?  5 : \
+        (BUILD_MONTH_IS_JUN) ?  6 : \
+        (BUILD_MONTH_IS_JUL) ?  7 : \
+        (BUILD_MONTH_IS_AUG) ?  8 : \
+        (BUILD_MONTH_IS_SEP) ?  9 : \
+        (BUILD_MONTH_IS_OCT) ? 10 : \
+        (BUILD_MONTH_IS_NOV) ? 11 : \
+        (BUILD_MONTH_IS_DEC) ? 12 : \
+        /* error default */  99 \
+    )
+
+#define COMPUTE_BUILD_HOUR ((__TIME__[0] - '0') * 10 + __TIME__[1] - '0')
+#define COMPUTE_BUILD_MIN  ((__TIME__[3] - '0') * 10 + __TIME__[4] - '0')
+#define COMPUTE_BUILD_SEC  ((__TIME__[6] - '0') * 10 + __TIME__[7] - '0')
+
+#define BUILD_DATE_IS_BAD (__DATE__[0] == '?')
+
+#define BUILD_YEAR  ((BUILD_DATE_IS_BAD) ? 99 : COMPUTE_BUILD_YEAR)
+#define BUILD_MONTH ((BUILD_DATE_IS_BAD) ? 99 : COMPUTE_BUILD_MONTH)
+#define BUILD_DAY   ((BUILD_DATE_IS_BAD) ? 99 : COMPUTE_BUILD_DAY)
+
+#define BUILD_TIME_IS_BAD (__TIME__[0] == '?')
+
+#define BUILD_HOUR  ((BUILD_TIME_IS_BAD) ? 99 :  COMPUTE_BUILD_HOUR)
+#define BUILD_MIN   ((BUILD_TIME_IS_BAD) ? 99 :  COMPUTE_BUILD_MIN)
+#define BUILD_SEC   ((BUILD_TIME_IS_BAD) ? 99 :  COMPUTE_BUILD_SEC)
+
+const char buildDateTime[] =
+{
+  BUILD_YEAR_CH0, BUILD_YEAR_CH1, BUILD_YEAR_CH2, BUILD_YEAR_CH3,
+  '-',
+  BUILD_MONTH_CH0, BUILD_MONTH_CH1,
+  '-',
+  BUILD_DAY_CH0, BUILD_DAY_CH1,
+  'T',
+  BUILD_HOUR_CH0, BUILD_HOUR_CH1,
+  ':',
+  BUILD_MIN_CH0, BUILD_MIN_CH1,
+  ':',
+  BUILD_SEC_CH0, BUILD_SEC_CH1,
+  '\0'
+};
+
+
+const char buildDateYYYYMMDD[] =
+{
+  BUILD_YEAR_CH0, BUILD_YEAR_CH1, BUILD_YEAR_CH2, BUILD_YEAR_CH3,
+  '-',
+  BUILD_MONTH_CH0, BUILD_MONTH_CH1,
+  '-',
+  BUILD_DAY_CH0, BUILD_DAY_CH1,
+  '\0'
+};
+
 static const char * get_build_date()
 {
     return __DATE__;
+}
+static const char * get_build_date_time()
+{
+  return buildDateTime;
+}
+
+static const char * get_build_dateYYYYMMDD()
+{
+  return buildDateYYYYMMDD;
 }
 #else
 static const char * get_build_date()
 {
     return("date not available");
+}
+static const char * get_build_date_time()
+{
+  return("date not available");
+}
+
+static const char * get_build_dateYYYYMMDD()
+{
+  return("date not available");
 }
 #endif
 
@@ -212,6 +377,8 @@ int do_json_info=0;
 int do_semver_info=0;
 int do_user_friendly_info=0;
 int do_build_date_only=0;
+int do_build_date_time_only=0;
+int do_build_date_yyyymmdd=0;
 int do_new_revision_only=0;
 int do_git_only=0;
 VersatileEncodingConfig vec=VEC_DEFAULT;
@@ -223,6 +390,8 @@ while (EOF!=(val=options.parse_long(argc,argv,optstring_VersionInfo,lopts_Versio
    case 'c': do_copyright_only=1; break;
    case 'r': do_revision_only=1; break;
    case 'b': do_build_date_only = 1; break;
+   case 't': do_build_date_time_only = 1; break;
+   case 'B': do_build_date_yyyymmdd = 1; break;
    case 'n': do_new_revision_only = 1; break;
    case 'g': do_git_only = 1; break;
    case 'R': retValue = get_unitex_revision(); break;
@@ -318,6 +487,12 @@ else if (do_git_only) {
 }
 else if (do_build_date_only) {
     u_sprintf(DisplayText,"%s", get_build_date());
+}
+else if (do_build_date_time_only) {
+  u_sprintf(DisplayText, "%s", get_build_date_time());
+}
+else if (do_build_date_yyyymmdd) {
+  u_sprintf(DisplayText, "%s", get_build_dateYYYYMMDD());
 }
 else if (do_version_only) {
     u_sprintf(DisplayText,"%u.%u",unitexMajorVersion,unitexMinorVersion);
