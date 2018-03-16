2d3cdd1307ca3bba384b2aa968d7035e0c9f4e22
Cristian Martinez <me@martinec.org>
2018-03-16 12:51:38 +0100

    changes

--- base/time/chrono.cpp
+++ base/time/chrono.cpp
@@ -1,7 +1,7 @@
 /*
  * Unitex
  *
- * Copyright (C) 2001-2017 Université Paris-Est Marne-la-Vallée <unitex-devel@univ-mlv.fr>
+ * Copyright (C) 2001-2018 Université Paris-Est Marne-la-Vallée <unitex-devel@univ-mlv.fr>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
