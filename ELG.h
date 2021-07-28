4a7f0e515c0f63dd7d03fbc973c5069f013642a0
Cristian Martinez <me@martinec.org>
2021-07-29 01:26:34 +0200

    [minor refactor] Add missing author information

--- ELG.h
+++ ELG.h
@@ -17,6 +17,8 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  *
+ * cristian.martinez@univ-paris-est.fr (martinec)
+ *
  */
 #ifndef ELG_H_
 #define ELG_H_
--- ELG_API.h
+++ ELG_API.h
@@ -17,6 +17,8 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  *
+ * cristian.martinez@univ-paris-est.fr (martinec)
+ *
  */
 #ifndef ELG_API_H_
 #define ELG_API_H_
